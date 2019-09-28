4de14ae550d9112cf5a937b61479145168aab3dd
pcc <pierre.cardona@untienots.com>
2019-09-28 15:52:14 +0200

    add project structre

---
+++ seraph/__main__.py
@@ -0,0 +1,6 @@
+"""Project Main
+"""
+from sample import app
+
+if __name__ == "__main__":
+    app.run()
---
+++ seraph/app.py
@@ -0,0 +1,9 @@
+"""Application run definition
+"""
+
+
+def run():  # pragma: no cover
+    """run function for the app
+    Returns:
+        None
+    """
